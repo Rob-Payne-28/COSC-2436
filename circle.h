b94fec1f391d2e6e9b20e0c090fa3f417e8167df
Rob Payne <robert.h.payne28.mil@army.mil>
2023-01-31 08:31:24 -0600

    Define attributes, getters, setters, and constructors in circle.h

--- circle.h
+++ circle.h
@@ -8,8 +8,28 @@ Purpose: Assignment 03 - "Classes and Objects" program that is part of COSC-2436
 #ifndef CLASSES_CIRCLE_H
 #define CLASSES_CIRCLE_H
 
+#define MIN 0.0
+#define PI 3.141592
+
 class Circle {
 
+public:
+    Circle();
+
+    Circle(double);
+
+    ~Circle();
+
+    double getCircumference();
+    double getRadius();
+    double getDiameter();
+    double getSurfaceArea();
+
+    void setCircumference(double);
+    void setRadius(double);
+
+protected:
+    double circumference, radius;
 };
 
 
