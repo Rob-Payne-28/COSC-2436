64a70069483e725a96abb554576edc4cc210f1b5
Rob Payne <robert.h.payne28.mil@army.mil>
2023-02-04 14:20:58 -0600

    Add labels to print dialog in functions.cpp for clarity

--- functions.cpp
+++ functions.cpp
@@ -7,18 +7,20 @@ Purpose: Assignment 03 - "Classes and Objects" program that is part of COSC-2436
 #include "functions.h"
 
 void displayCircle(Circle *myCircle){
-    std::cout << myCircle->getRadius() << std::endl;
-    std::cout << myCircle->getDiameter() << std::endl;
-    std::cout << myCircle->getCircumference() << std::endl;
-    std::cout << myCircle->getArea() << std::endl;
+    std::cout << "Diplaying Circle- " << std::endl;
+    std::cout << " Radius: " << myCircle->getRadius() << std::endl;
+    std::cout << " Diameter: " << myCircle->getDiameter() << std::endl;
+    std::cout << " Circumference: " << myCircle->getCircumference() << std::endl;
+    std::cout << " Area: " << myCircle->getArea() << std::endl;
     std::cout << std::endl;
 }
 
 void displayCylinder(Cylinder *myCylinder){
-    std::cout << myCylinder->getRadius() << std::endl;
-    std::cout << myCylinder->getDiameter() << std::endl;
-    std::cout << myCylinder->getCircumference() << std::endl;
-    std::cout << myCylinder->getArea() << std::endl;
-    std::cout << myCylinder->getVolume() << std::endl;
+    std::cout << "Displaying Cylinder- " << std::endl;
+    std::cout << " Radius: " << myCylinder->getRadius() << std::endl;
+    std::cout << " Diameter: " << myCylinder->getDiameter() << std::endl;
+    std::cout << " Circumference: " << myCylinder->getCircumference() << std::endl;
+    std::cout << " Area: " << myCylinder->getArea() << std::endl;
+    std::cout << " Volume: " << myCylinder->getVolume() << std::endl;
     std::cout << std::endl;
 }
