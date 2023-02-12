1733e4da42bacfda2ffb3743925d1785921ce35a
Rob Payne <robert.h.payne28.mil@army.mil>
2023-02-12 15:27:19 -0600

    Add peek and pop tests for underflow condition and remove unused constant

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ Purpose: Assignment 04 - Simple Int Stack program that is part of COSC-2436.
 **********************/
 
 #include "main.h"
-#define SIZE_TOO_SMALL 1
 #define SIZE 10
 #define TEST_RUNS 10
 
@@ -75,45 +74,66 @@ int main(int argc, char** argv) {
     }
     std::cout << std::endl;
 
-
-    // Runs tests in random order
-    std::cout << "Testing methods in a random order" << std::endl;
+    // Tests pop in underflow
+    std::cout << "Testing pop in underflow" << std::endl;
     for (int i = 0; i < TEST_RUNS; ++i) {
-        //generates random number with 4 possible outcomes (0-3)
-        int randomNum = rand() % 4;
-        int pushNum;
-        switch (randomNum) {
-            case 0:
-                if(stack.isEmpty()){
-                    std::cout << "Stack is currently empty" << std::endl;
-                } else {
-                    std::cout << "Stack is not empty" << std::endl;
-                }
-                break;
-            case 1:
-                if(stack.isEmpty()){
-                    std::cout << "Cannot pop! Stack in underflow state!" << std::endl;
-                } else {
-                    std::cout << "Popping " << stack.pop() << std::endl;
-                }
-                break;
-            case 2:
-                pushNum = rand() % 25;
-                if(stack.push(pushNum)){
-                    std::cout << "Pushed: " << pushNum << std::endl;
-                } else {
-                    std::cout << "Cannot push! Stack in overflow state!" << std::endl;
-                }
-                break;
-            case 3:
-                if(stack.peek() == -1){
-                    std::cout << "Stack is currently empty! Cannot peek!" << std::endl;
-                } else {
-                    std::cout << "Peeking: " << stack.peek() << std::endl;
-                }
-                break;
+        try {
+            std::cout << "Popping: " << stack.pop() << std::endl;
+        } catch (const std::runtime_error& error) {
+            std::cerr << error.what() << std::endl;
         }
     }
+    std::cout << std::endl;
+
+    // Tests peek in underflow
+    std::cout << "Testing peek in underflow" << std::endl;
+    for (int i = 0; i < TEST_RUNS; ++i) {
+        try {
+            std::cout << "Peeking: " << stack.peek() << std::endl;
+        } catch (const std::runtime_error& error) {
+            std::cerr << error.what() << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
+//    // Runs tests in random order
+//    std::cout << "Testing methods in a random order" << std::endl;
+//    for (int i = 0; i < TEST_RUNS; ++i) {
+//        //generates random number with 4 possible outcomes (0-3)
+//        int randomNum = rand() % 4;
+//        int pushNum;
+//        switch (randomNum) {
+//            case 0:
+//                if(stack.isEmpty()){
+//                    std::cout << "Stack is currently empty" << std::endl;
+//                } else {
+//                    std::cout << "Stack is not empty" << std::endl;
+//                }
+//                break;
+//            case 1:
+//                if(stack.isEmpty()){
+//                    std::cout << "Cannot pop! Stack in underflow state!" << std::endl;
+//                } else {
+//                    std::cout << "Popping " << stack.pop() << std::endl;
+//                }
+//                break;
+//            case 2:
+//                pushNum = rand() % 25;
+//                if(stack.push(pushNum)){
+//                    std::cout << "Pushed: " << pushNum << std::endl;
+//                } else {
+//                    std::cout << "Cannot push! Stack in overflow state!" << std::endl;
+//                }
+//                break;
+//            case 3:
+//                if(stack.peek() == -1){
+//                    std::cout << "Stack is currently empty! Cannot peek!" << std::endl;
+//                } else {
+//                    std::cout << "Peeking: " << stack.peek() << std::endl;
+//                }
+//                break;
+//        }
+//    }
 
     return 0;
 }
