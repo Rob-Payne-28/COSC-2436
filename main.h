d0f28107c8fe45bd3af68a3ca743367adba09b2e
Rob Payne <robert.h.payne28.mil@army.mil>
2023-02-11 23:28:24 -0600

    Add comments for header file includes statements

--- main.h
+++ main.h
@@ -8,7 +8,10 @@ Purpose: Assignment 04 - Simple Int Stack program that is part of COSC-2436.
 #define STACK_MAIN_H
 
 #include <iostream>
+// Adds rand function
 #include <cstdlib>
+// Adds time function used for random seed generation
+#include <ctime>
 #include "stack.h"
 
 #endif //STACK_MAIN_H
