1876a5100712a2842746a1048865e2e2d322dd49
Rob Payne <robert.h.payne28.mil@army.mil>
2023-02-25 11:51:12 -0600

    Create scaffolding for stack class in stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -4,9 +4,33 @@ Coding 05
 Purpose: Assignment 05 - Stack program that is part of COSC-2436.
 **********************/
 
-// each cpp includes ONLY it's header directly
 #include "stack.h"
 
-/*
- * write all your stack methods here
- */
+Stack::Stack(int customSize) {
+    top = -1;
+    size = customSize;
+}
+
+int Stack::getSize() {
+    return size;
+}
+
+bool Stack::isEmpty() {
+    return top < 0;
+}
+
+bool Stack::peek() {
+    // ToDo: Make peek function
+    return true;
+}
+
+bool Stack::pop() {
+    // ToDo: Make pop function
+    return true;
+}
+
+bool Stack::push() {
+    // ToDo: Make push function
+    return true;
+}
+
