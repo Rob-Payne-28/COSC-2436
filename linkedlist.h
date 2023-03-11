560941d164bc1a31b84bcbb5067827090e4fb485
Rob Payne <robert.h.payne28.mil@army.mil>
2023-03-11 12:03:26 -0600

    Add linkedlist.h structure

--- linkedlist.h
+++ linkedlist.h
@@ -3,3 +3,20 @@ Name: Robert Payne
 Coding 06
 Purpose: Assignment 06 - Doubly Linked List that is part of COSC-2436.
 **********************/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include "data.h"
+
+class LinkedList {
+
+public:
+
+
+private:
+    
+
+};
+
+#endif //LINKED_LIST_H
