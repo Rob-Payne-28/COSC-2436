6d1f80f70243d57aa258995dfef967e42724e7bf
Rob Payne <robert.h.payne28.mil@army.mil>
2023-04-01 10:18:18 -0500

    Add boilerplate for hashtable.h

--- hashtable.h
+++ hashtable.h
@@ -3,3 +3,20 @@ Name: Robert Payne
 Coding 07
 Purpose: Assignment 07 - Hash Table backed by Doubly Linked List that is part of COSC-2436.
 **********************/
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include "data.h"
+
+class HashTable {
+
+public:
+
+
+private:
+
+
+};
+
+#endif //HASH_TABLE_H
