eec15b31104a1dcbc96314ab9be0ff19dfffba4c
Rob Payne <robert.h.payne28.mil@army.mil>
2023-04-01 22:28:51 -0500

    Add edge case testing

--- main.cpp
+++ main.cpp
@@ -51,14 +51,19 @@ int main() {
     
     HashTable hashTable;
 
+    // These tests will exercise the methods as they're intended to be used
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "=====================================================" << std::endl;
     std::cout << "Executing method tests..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "=====================================================" << std::endl;
     std::cout << std::endl;
 
     std::cout << "=====================================================" << std::endl;
     std::cout << "            Getting count..." << std::endl;
     std::cout << "=====================================================" << std::endl;
     std::cout << std::endl;
-    std::cout << "Number of entries: " << hashTable.getCount() << std::endl;
+    std::cout << " Number of entries: " << hashTable.getCount() << std::endl;
     std::cout << std::endl;
 
     std::cout << "=====================================================" << std::endl;
@@ -66,21 +71,6 @@ int main() {
     std::cout << "=====================================================" << std::endl;
     std::cout << std::endl;
     hashTable.printTable();
-
-    std::cout << "=====================================================" << std::endl;
-    std::cout << "            Getting data on empty table..." << std::endl;
-    std::cout << "=====================================================" << std::endl;
-    std::cout << std::endl;
-
-    for (int i = ZERO; i < testdatasize; i++) {
-        std::string data = hashTable.getData(ids[i]);
-        if (!data.empty()) {
-            std::cout << "Retrieved data for id " << ids[i] << ": \'" << data << "\'" << std::endl;
-        } else {
-            std::cout << "No data found for id " << ids[i] << std::endl;
-        }
-    }
-    std::cout << std::endl;
 
     std::cout << "=====================================================" << std::endl;
     std::cout << "            Filling table..." << std::endl;
@@ -101,7 +91,7 @@ int main() {
     std::cout << "            Getting count..." << std::endl;
     std::cout << "=====================================================" << std::endl;
     std::cout << std::endl;
-    std::cout << "Number of entries: " << hashTable.getCount() << std::endl;
+    std::cout << " Number of entries: " << hashTable.getCount() << std::endl;
     std::cout << std::endl;
 
     std::cout << "=====================================================" << std::endl;
@@ -118,9 +108,9 @@ int main() {
     for (int i = ZERO; i < testdatasize; i++) {
         std::string data = hashTable.getData(ids[i]);
         if (!data.empty()) {
-            std::cout << "Retrieved data for id " << ids[i] << ": \'" << data << "\'" << std::endl;
+            std::cout << " Retrieved data for id " << ids[i] << ": \'" << data << "\'" << std::endl;
         } else {
-            std::cout << "No data found for id " << ids[i] << std::endl;
+            std::cout << " No data found for id " << ids[i] << std::endl;
         }
     }
     std::cout << std::endl;
@@ -145,7 +135,7 @@ int main() {
     std::cout << "=====================================================" << std::endl;
     std::cout << std::endl;
 
-    std::cout << "Number of entries: " << hashTable.getCount() << std::endl;
+    std::cout << " Number of entries: " << hashTable.getCount() << std::endl;
     std::cout << std::endl;
 
     std::cout << "=====================================================" << std::endl;
@@ -155,15 +145,113 @@ int main() {
     hashTable.printTable();
 
 
+    /*
+     * These tests will exercise methods in ways in which we wouldn't expect them to
+     * be used, such as attempting duplicate entries, getting data from an empty table,
+     * or removing from an empty table.
+    */
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << " Executing edge-case tests..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Removing from empty table..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    for (int i = ZERO; i < testdatasize; i++) {
+        bool success = hashTable.removeEntry(ids[i]);
+        if (success) {
+            std::cout << " Removed the entry with id " << ids[i] << std::endl;
+        } else {
+            std::cout << " Failed to remove the entry with id " << ids[i] << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Getting data on empty table..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    for (int i = ZERO; i < testdatasize; i++) {
+        std::string data = hashTable.getData(ids[i]);
+        if (!data.empty()) {
+            std::cout << " Retrieved data for id " << ids[i] << ": \'" << data << "\'" << std::endl;
+        } else {
+            std::cout << " No data found for id " << ids[i] << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Filling table for full tests..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    for (int i = ZERO; i < testdatasize; i++) {
+        hashTable.insertEntry(ids[i], &strs[i]);
+    }
+    std::cout << " Table filled with test data for full edge case tests" << std::endl;
+    std::cout << std::endl;
+    std::cout << " Number of entries: " << hashTable.getCount() << std::endl;
+    std::cout << std::endl;
+    hashTable.printTable();
+
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Attempting to add duplicates..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    for (int i = ZERO; i < testdatasize; i++) {
+        bool success = hashTable.insertEntry(ids[i], &strs[i]);
+        if (success) {
+            std::cout << " Inserted entry with id " << ids[i] << " and data \'" << strs[i] << "\'" << std::endl;
+        } else {
+            std::cout << " Failed to insert entry with id " << ids[i] << " and data \'" << strs[i] << "\'" << std::endl;
+        }
+    }
+    std::cout << std::endl;
 
 
-    // continue using and testing your table, add and remove data,
-    // do whatever it takes to full test your object and prove it
-    // is robust and can handle all use cases.
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Attempting to add invalid numbers..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
 
+    for (int i = ZERO; i > -HASH_TABLE_SIZE; i--) {
+        Data data;
+        data.id = i;
+        data.data = strs[-i];
+        bool success = hashTable.insertEntry(i, &data.data);
+        if (success) {
+            std::cout << " Inserted entry with id " << i << " and data \'" << data.data << "\'" << std::endl;
+        } else {
+            std::cout << " Failed to insert entry with id " << i << " and data \'" << data.data << "\'" << std::endl;
+        }
+    }
+    std::cout << std::endl;
 
-    
-    
-    
+    std::cout << "=====================================================" << std::endl;
+    std::cout << "            Attempting to add empty strings..." << std::endl;
+    std::cout << "=====================================================" << std::endl;
+    std::cout << std::endl;
+
+    for (int i = ZERO; i < testdatasize; i++) {
+        Data data;
+        data.id = ids[i];
+        data.data = EMPTY_STRING;
+        bool success = hashTable.insertEntry(data.id, &data.data);
+        if (success) {
+            std::cout << " Inserted entry with id " << data.id << " and data \'" << data.data << "\'" << std::endl;
+        } else {
+            std::cout << " Failed to insert entry with id " << data.id << " and data \'" << data.data << "\'" << std::endl;
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
