7fed0bcaaab23280afc42a55e51dae6112f879a9
Rob Payne <robert.h.payne28.mil@army.mil>
2023-04-12 13:12:25 -0500

    Stub methods so program compiles

--- bintree.cpp
+++ bintree.cpp
@@ -6,4 +6,103 @@ Purpose: Assignment 07 - Binary Search Tree that is part of COSC-2436.
 
 #include "bintree.h"
 
+BinTree::BinTree() {
+
+};
+
+BinTree::~BinTree() {
+
+};
+
+bool BinTree::isEmpty() {
+    return false;
+};
+
+int BinTree::getCount() {
+    return 0;
+};
+
+bool BinTree::getRootData(Data*) {
+    return false;
+};
+
+void BinTree::displayTree() {
+
+};
+
+
+void BinTree::clear() {
+
+};
+
+void BinTree::clear(DataNode*) {
+
+};
+
+bool BinTree::addNode(int, const string*) {
+    return false;
+};
+
+bool BinTree::addNode(DataNode*, DataNode**) {
+    return false;
+};
+
+bool BinTree::removeNode(int) {
+    return false;
+};
+
+DataNode* BinTree::removeNode(int id, DataNode* dataNode) {
+    DataNode *node = new DataNode();
+    return node;
+};
+
+int BinTree::getHeight() {
+    return 0;
+};
+
+int BinTree::getHeight(DataNode*) {
+    return 0;
+};
+
+bool BinTree::getNode(Data*, int) {
+    return false;
+};
+
+bool BinTree::getNode(Data*, int, DataNode*) {
+    return false;
+};
+
+bool BinTree::contains(int) {
+    return false;
+};
+
+bool BinTree::contains(int, DataNode*) {
+    return false;
+};
+
+
+void BinTree::displayPreOrder() {
+
+};
+
+void BinTree::displayPreOrder(DataNode*) {
+
+};
+
+void BinTree::displayPostOrder() {
+
+};
+
+void BinTree::displayPostOrder(DataNode*) {
+
+};
+
+void BinTree::displayInOrder() {
+
+};
+
+void BinTree::displayInOrder(DataNode*) {
+
+};
+
 
