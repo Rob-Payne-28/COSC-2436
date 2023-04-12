f3d110f0c950c7bd79e51be17a0502a985f59931
Rob Payne <robert.h.payne28.mil@army.mil>
2023-04-12 11:53:00 -0500

    Add comment headers

--- bintree.cpp
+++ bintree.cpp
@@ -1,6 +1,8 @@
-/*
- * Your comment header here
- */
+/*********************
+Name: Robert Payne
+Coding 07
+Purpose: Assignment 07 - Binary Search Tree that is part of COSC-2436.
+**********************/
 
 #include "bintree.h"
 
