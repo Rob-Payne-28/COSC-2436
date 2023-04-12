b922ab4954278776c5f3b2d1e35a33d6a1105200
Rob Payne <robert.h.payne28.mil@army.mil>
2023-04-12 11:59:36 -0500

    Create public interface for BinTree.h

--- bintree.h
+++ bintree.h
@@ -7,7 +7,19 @@ Purpose: Assignment 07 - Binary Search Tree that is part of COSC-2436.
 #ifndef BINTREE_BINTREE_H
 #define BINTREE_BINTREE_H
 
+class BinTree {
 
+public:
+    BinTree();
+    ~BinTree();
+    bool isEmpty();
+    int getCount();
+    bool getRootData(Data*);
+    void displayTree();
+
+private:
+
+};
 
 
 #endif /* BINTREE_BINTREE_H */
