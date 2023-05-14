1e22c3c79a771fe6099f386193d35c5656d55c73
Rob Payne <robert.h.payne28.mil@army.mil>
2023-05-13 22:43:35 -0500

    Add scaffolding for graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -7,26 +7,66 @@ Purpose: Assignment 10 - Graph Abstract Data Type that is the final project for 
 #include "graph.h"
 
 // ToDo - Constructor
+Graph::Graph() {}
 // ToDo - Deconstructor
+Graph::~Graph() {}
 
 // Methods to build and remove nodes/edges
-// ToDo - addVertex
-// ToDo - removeVertex
-// ToDo - addEdge
-// ToDo - removeEdge
+bool Graph::addVertex(int, std::string) {
+    // ToDo - addVertex
+}
+
+bool Graph::removeVertex(int) {
+    // ToDo - removeVertex
+}
+
+bool Graph::addEdge(int, int) {
+    // ToDo - addEdge
+}
+
+bool Graph::removeEdge(int, int) {
+    // ToDo - removeEdge
+}
 
 // Methods to find things
-// ToDo - getVertices
-// ToDo - getEdges
-// ToDo - getAdjacent
+bool Graph::getVertices(vector <Vertex> &) {
+    // ToDo - getVertices
+}
+
+bool Graph::getEdges(vector <Edge> &) {
+    // ToDo - getEdges
+}
+
+bool Graph::getAdjacent(int, vector <Vertex> &) {
+    // ToDo - getAdjacent
+}
 
 // Helper methods (called by other methods to reduce complexity)
-// ToDo - hasVertex (should this be private?)
-// ToDo - hasEdge (should this be private?)
-// ToDo - size
+bool Graph::hasVertex(int) {
+    // ToDo - hasVertex
+}
+
+bool Graph::hasEdge(int, int) {
+    // ToDo - hasEdge
+}
+
+int Graph::size() {
+    // ToDo - size
+}
 
 // Not sure if we're going to need these, but they'd be nice to add if we have time
-// ToDo - BFS
-// ToDo - DFS
-// ToDo - printAdjacencyMatrix
-// ToDo - printAdjacencyList
+bool Graph::breadthFirstSearch(int, vector <Vertex> &) {
+    // ToDo - BFS
+}
+
+bool Graph::depthFirstSearch(int, vector <Vertex> &) {
+    // ToDo - DFS
+}
+
+void Graph::printAdjacencyMatrix() {
+    // ToDo - printAdjacencyMatrix
+}
+
+void Graph::printAdjacencyList() {
+    // ToDo - printAdjacencyList
+}
