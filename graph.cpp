915a06c9c2504c05fab430e5d03f2b1738d29a61
Rob Payne <robert.h.payne28.mil@army.mil>
2023-05-13 19:14:58 -0500

    Add graph interface with todos and graph implementation

---
+++ graph.cpp
@@ -0,0 +1,8 @@
+/*********************
+Name: Robert Payne
+Coding 10
+Purpose: Assignment 10 - Graph Abstract Data Type that is the final project for COSC-2436.
+**********************/
+
+#include "graph.h"
+
---
+++ graph.h
@@ -0,0 +1,45 @@
+/*********************
+Name: Robert Payne
+Coding 10
+Purpose: Assignment 10 - Graph Abstract Data Type that is the final project for COSC-2436.
+**********************/
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+class graph {
+
+public:
+    // ToDo - Constructor
+    // ToDo - Deconstructor
+
+    // Methods to build and remove nodes/edges
+    // ToDo - addVertex
+    // ToDo - removeVertex
+    // ToDo - addEdge
+    // ToDo - removeEdge
+
+    // Methods to find things
+    // ToDo - getVertices
+    // ToDo - getEdges
+    // ToDo - getAdjacent
+
+    // Helper methods (called by other methods to reduce complexity)
+    // ToDo - hasVertex (should this be private?)
+    // ToDo - hasEdge (should this be private?)
+    // ToDo - size
+
+    // Not sure if we're going to need these, but they'd be nice to add if we have time
+    // ToDo - BFS
+    // ToDo - DFS
+
+private:
+    // Member variables
+    // ToDo - Member variable for vertices list (possibly a map or vector?)
+    // ToDo - Member variable for the adjacency list (possibly a map or a vector?)
+
+    // Private methods (May move helper methods here)
+
+};
+
+#endif //GRAPH_H
