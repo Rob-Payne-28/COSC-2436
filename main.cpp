b6cd46c427350f7d518a4a95cd5112a58ba97117
Rob Payne <robert.h.payne28.mil@army.mil>
2023-05-13 19:03:43 -0500

    Add main class and header file scaffolding

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*********************
+Name: Robert Payne
+Coding 10
+Purpose: Assignment 10 - Graph Abstract Data Type that is the final project for COSC-2436.
+**********************/
+
+#include "main.h"
+
+int main() {
+
+    // ToDo - Add test data generation
+
+    // ToDo - Add method testing
+
+    // ToDo - Add edge-case testing
+
+    // ToDo - Add random testing
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,12 @@
+/*********************
+Name: Robert Payne
+Coding 10
+Purpose: Assignment 10 - Graph Abstract Data Type that is the final project for COSC-2436.
+**********************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+#endif //MAIN_H
